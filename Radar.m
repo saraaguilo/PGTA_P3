9cf38587cc9e4ca1ea47ea8965811a5b7fe72142
saraaguilo <sara5aguilo@gmail.com>
2024-04-22 17:08:19 +0200

    Pérdidas de separación según Radar

---
+++ Radar.m
@@ -0,0 +1,23 @@
+% Uno de los parámetros ATM de interés es: pérdidas de separación entre
+% despegues consecutivos según Radar
+
+function Radar(flightplan, asterix)
+
+% Departure time components
+departuretimeinHours = [flightplan.HoraDespegue.Hour];
+departuretimeinMinutes = [flightplan.HoraDespegue.Minute];
+departuretimeinSeconds = [flightplan.HoraDespegue.Second];
+
+[~, idx_flightplan, idx_asterix] = intersect([flightplan.id, departuretimeinHours, departuretimeinMinutes, departuretimeinSeconds], ...
+                                             [asterix.I240_TId, asterix.Hours, asterix.Minutes, asterix.Seconds], 'rows');
+
+% Calculating distance for matching messages
+x1 = asterix.X(idx_asterix);
+y1 = asterix.Y(idx_asterix);
+x2 = flightplan.X(idx_flightplan);
+y2 = flightplan.Y(idx_flightplan);
+distance = sqrt((x2 - x1).^2 + (y2 - y1).^2);
+
+flightplan.Distance(idx_flightplan) = distance;
+
+end
